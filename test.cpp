fb5f4a43b2248df6d8bda461ebfca820d09d8bfe
crisis-argha <namespace.arg@gmail.com>
2023-05-30 19:41:33 +0530

    array

--- test.cpp
+++ test.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 int main()
 {
-   map<int, string> m;
-   m[1] = "argha";
-   m[6] = "crisis";
-   m[2] = "cross";
-   m[3] = "yoo";
-   auto it = m.find(6);
-   cout << (*it).second;
+   map<int, int> m;
+   m[1] = 30;
+   m[6] = 40;
+   m[2] = 50;
+   m[3] = 60;
+   for (auto it : m)
+   {
+      it.second++;
+      cout << it.second << endl;
+   }
 }
