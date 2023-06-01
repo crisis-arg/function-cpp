edb0fa4dc567d9f5796e28a746f8a78483f16879
crisis-argha <namespace.arg@gmail.com>
2023-06-01 20:11:10 +0530

    Deleting the Node at nth position in a single linked list

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ struct node
    int data;
    node *next;
 };
-node *head;
+ node *head;
 void insert(int x)
 {
    node *temp1 = (node *)malloc(sizeof(int));
@@ -36,6 +36,25 @@ void print()
    }
    cout << endl;
 }
+void Delete(int n)
+{
+   node *temp1 = head;
+   node *temp2 = head;
+   if (n == 1)
+   {
+      head = (*temp1).next;
+      delete (temp1);
+      return;
+   }
+   while (n != 1)
+   {
+      temp2 = temp1;
+      temp1 = (*temp1).next;
+      n--;
+   }
+   (*temp2).next = (*temp1).next;
+   free(temp1);
+}
 int main()
 {
    head = NULL;
@@ -43,7 +62,12 @@ int main()
    insert(4);
    insert(7);
    insert(3);
-   insert(6);
+   insert(5);
    insert(9);
+   insert(40);
+   print();
+   int n;
+   cin >> n;
+   Delete(n);
    print();
 }
