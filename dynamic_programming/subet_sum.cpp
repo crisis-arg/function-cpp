fb0230244273bc44de2446d619e124a95caebc03
crisis-argha <namespace.arg@gmail.com>
2023-06-05 21:29:09 +0530

    subset sum problem using dp

---
+++ dynamic_programming/subet_sum.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool subset_sum(int arr[], int sum, int n)
+{
+    vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1));
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = 0; j < sum + 1; j++)
+        {
+            if (i == 0)
+            {
+                dp[i][j] = false;
+            }
+            if (j == 0)
+            {
+                dp[i][j] = true;
+            }
+            else if (arr[i - 1] <= j)
+            {
+                dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
+            }
+            else if (arr[i - 1] > j)
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    return dp[n][sum];
+}
+int main()
+{
+    int n;
+    cout << "enter how many numbers: " << endl;
+    cin >> n;
+    int arr[n];
+    cout << "enter the numbers: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int target;
+    cout << "enter the sum: " << endl;
+    cin >> target;
+    cout << subset_sum(arr, target, n);
+}
--- test.cpp
+++ test.cpp
@@ -1,48 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-// vector<vector<int>> dp(10, vector<int>(10,-1));
-int knapsack(int val[], int wt[], int w, int n)
+bool subset_sum(int arr[], int sum, int n)
 {
-    vector<vector<int>> dp(n+1, vector<int>(w+1,0));
-    for (int i = 0; i < n + 1; i++)
+  vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1));
+  for (int i = 0; i < n + 1; i++)
+  {
+    for (int j = 0; j < sum + 1; j++)
     {
-        for (int j = 0; j < w + 1; j++)
-        {
-            if (i == 0 || j == 0)
-            {
-                dp[i][j] = 0;
-            }
-            else if (wt[i - 1] <= j)
-            {
-                dp[i][j] = max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
-            }
-            else  if (wt[i - 1] > j)
-            {
-                dp[i][j] = dp[i - 1][j];
-            }
-        }
+      if (i == 0)
+      {
+        dp[i][j] = false;
+      }
+      if (j == 0)
+      {
+        dp[i][j] = true;
+      }
+      else if (arr[i - 1] <= j)
+      {
+        dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
+      }
+      else if (arr[i - 1] > j)
+      {
+        dp[i][j] = dp[i - 1][j];
+      }
     }
-    return dp[n][w];
+  }
+  return dp[n][sum];
 }
 int main()
 {
-    int n;
-    cout << "enter the total item num: " << endl;
-    cin >> n;
-    int val[n];
-    int wt[n];
-    int w;
-    cout << "enter the values of items: " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> val[i];
-    }
-    cout << "enter the weight of the items: " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> wt[i];
-    }
-    cout << "enter the weight of the knapsack: " << endl;
-    cin >> w;
-    cout << knapsack(val, wt, w, n);
+  int n;
+  cout << "enter how many numbers: " << endl;
+  cin >> n;
+  int arr[n];
+  cout << "enter the numbers: " << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+  int target;
+  cout << "enter the sum: " << endl;
+  cin >> target;
+  cout << subset_sum(arr, target, n);
 }
