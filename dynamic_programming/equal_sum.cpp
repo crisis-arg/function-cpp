339cb09428b6443b92e237f036169c26c837ce37
crisis-argha <namespace.arg@gmail.com>
2023-06-06 20:20:45 +0530

    Equal Sum Partition Problem using dp

---
+++ dynamic_programming/equal_sum.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool subset_sum(int arr[], int sum, int n)
+{
+    vector<vector<bool>> dp(n + 1, vector<bool>(sum + 1));
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = 0; j < sum + 1; j++)
+        {
+            if (i == 0)
+            {
+                dp[i][j] = false;
+            }
+            if (j == 0)
+            {
+                dp[i][j] = true;
+            }
+            else if (arr[i - 1] <= j)
+            {
+                dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
+            }
+            else if (arr[i - 1] > j)
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    return dp[n][sum];
+}
+bool equal_sum(int arr[], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+    }
+    if (sum % 2 != 0)
+    {
+        return false;
+    }
+    else
+    {
+        return subset_sum(arr, sum / 2, n);
+    }
+}
+int main()
+{
+    int n;
+    cout << "enter how many numbers: " << endl;
+    cin >> n;
+    int arr[n];
+    cout << "enter the numbers: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << equal_sum(arr, n);
+}
--- test.cpp
+++ test.cpp
@@ -27,6 +27,22 @@ bool subset_sum(int arr[], int sum, int n)
   }
   return dp[n][sum];
 }
+bool equal_sum(int arr[], int n)
+{
+  int sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    sum = sum + arr[i];
+  }
+  if (sum % 2 != 0)
+  {
+    return false;
+  }
+  else
+  {
+    return subset_sum(arr, sum / 2, n);
+  }
+}
 int main()
 {
   int n;
@@ -38,8 +54,5 @@ int main()
   {
     cin >> arr[i];
   }
-  int target;
-  cout << "enter the sum: " << endl;
-  cin >> target;
-  cout << subset_sum(arr, target, n);
+  cout << equal_sum(arr, n);
 }
