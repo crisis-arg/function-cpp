d83251b6e29dd3908c8dd99b58c4614451e1aa23
crisis-argha <namespace.arg@gmail.com>
2023-06-09 00:05:04 +0530

    leetcode 134.gas_station

---
+++ leetode/134.gas_station.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+int gas_station(int gas[], int cost[], int n)
+{
+    int sum_gas = 0;
+    int sum_cost = 0;
+    int left_gas = 0;
+    int start = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum_gas += gas[i];
+        sum_cost += cost[i];
+        if (sum_cost > sum_gas)
+        {
+            return -1;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        left_gas = gas[i] - cost[i];
+        if (left_gas < 0)
+        {
+            start = i + 1;
+        }
+    }
+    return start;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int gas[n];
+    int cost[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> gas[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> cost[i];
+    }
+    cout << gas_station(gas, cost, n);
+}
--- test.cpp
+++ test.cpp
@@ -1,64 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-int count_subset_sum(int arr[], int sum, int n)
+int gas_station(int gas[], int cost[], int n)
 {
-  vector<vector<int>> dp(n + 1, vector<int>(sum + 1));
-  for (int i = 0; i < n + 1; i++)
+  int sum_gas = 0;
+  int sum_cost = 0;
+  int left_gas = 0;
+  int start = 0;
+  for (int i=0; i < n; i++)
   {
-    for (int j = 0; j < sum + 1; j++)
+    sum_gas += gas[i];
+    sum_cost += cost[i];
+    if (sum_cost > sum_gas)
     {
-      if (j == 0)
-      {
-        dp[i][j] = 1;
-      }
-      if (i == 0)
-      {
-        if (j != 0)
-        {
-          dp[i][j] = 0;
-        }
-      }
-      else if (arr[i - 1] <= j)
-      {
-        dp[i][j] = dp[i - 1][j - arr[i - 1]] + dp[i - 1][j];
-      }
-      else if (arr[i - 1] > j)
-      {
-        dp[i][j] = dp[i - 1][j];
-      }
+      return -1;
     }
   }
-  return dp[n][sum];
-}
-// sum(s1)-sum(s2)=diff........(1)
-// sum(s1)+sum(s2)=sum(arr).......(2)
-// adding two equations (1)+(2)
-// 2*sum(1)=diff+sum(arr)
-// sum(s1)={diff+sum(arr)}/2
-// if I find the count of sum(s1), i find the count of the diff in the given array
-int diff_count(int arr[], int diff, int n)
-{
-  int arr_sum = 0;
   for (int i = 0; i < n; i++)
   {
-    arr_sum = arr_sum + arr[i];
+    left_gas = gas[i] - cost[i];
+    if (left_gas < 0)
+    {
+      start = i + 1;
+    }
   }
-  int sub1 = (diff + arr_sum) / 2;
-  return count_subset_sum(arr, sub1, n);
+  return start;
 }
 int main()
 {
   int n;
-  cout << "enter how many numbers: " << endl;
   cin >> n;
-  int arr[n];
-  cout << "enter the numbers: " << endl;
+  int gas[n];
+  int cost[n];
   for (int i = 0; i < n; i++)
   {
-    cin >> arr[i];
+    cin >> gas[i];
   }
-  int diff;
-  cout << "enter the diff: " << endl;
-  cin >> diff;
-  cout << diff_count(arr, diff, n);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> cost[i];
+  }
+  cout << gas_station(gas, cost, n);
 }
